0142149c2cc84d87b124ccd38e069a3d26635c7e
joenjihwan <s20062@gsm.hs.kr>
2020-12-14 10:52:22 +0900

    [UPDATE]Returns

---
+++ Theory/Returns.py
@@ -0,0 +1,14 @@
+def p_plus(a,b):
+    print(a+b)
+
+def r_plus(a,b):
+    return a+b
+    print("lalala")
+
+# p_result = p_plus(2,3)
+# r_result = r_plus(2,3)
+
+r_result = r_plus(2,3)
+
+print(r_result)
+# None으로 뜨는 이유는 p_result가 아무것도 return X
