2ccd6355c7dc1b77af5cd4d1b07827449322b9e1
joenjihwan <s20062@gsm.hs.kr>
2020-12-17 13:51:15 +0900

    [UPDATE]Modules and import def.

---
+++ Theory/Modules.py
@@ -0,0 +1,4 @@
+from math import fsum as sex #fsum을 sex로 지정.
+#import math 비효율적
+#print(ceil(1.2)) # 반올림
+print(sex([1,2,3,4,5]))
---
+++ Theory/main.py
@@ -0,0 +1,6 @@
+from calculator import plus, minus
+
+print(plus(1,2))
+print(minus(1,2))
+
+#calculator file download.
