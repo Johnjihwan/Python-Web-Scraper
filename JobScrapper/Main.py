27cbb8bdc408ff242ff7e4d057aa9396e5772e2b
joenjihwan <s20062@gsm.hs.kr>
2020-12-18 12:30:58 +0900

    [UPDATE]print data with array

--- JobScrapper/Main.py
+++ JobScrapper/Main.py
@@ -7,6 +7,11 @@ job_soup = BeautifulSoup(job_result.text, "html.parser")
 
 grid_main_banner_A = job_soup.find('ul', {"class" : "grid_main_banner_A"})
 
-pages = grid_main_banner_A.find_all('h2') #h2 tag인 것만 pages에 저장
+links = grid_main_banner_A.find_all('h2') #h2 tag인 것만 pages에 저장
+pages = []
 
-print(pages[0:5])
+for link in links:
+    pages.append(link.string)
+
+pages = pages[0:-1]
+print(pages)
